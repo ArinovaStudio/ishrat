809596f04694f2c29a059b364dcc1cc5454be8df
Arinova Studio <studio.arinova@gmail.com>
2026-02-22 18:18:39 +0530

    Update projectId in sanity.cli.ts

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'iy6682ud',
+    projectId: '21iau7yh',
     dataset: 'production'
   },
   deployment: {
